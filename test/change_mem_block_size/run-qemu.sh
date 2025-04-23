085ce15ac7993a75e95391df4cbf50e7911c5a1a
Yusun Choi <tanks2438@outlook.kr>
2025-04-23 20:28:47 +0900

    test change_mem_block_size: Change run-qemu.sh and patch file
    Signed-off-by: Yusun Choi <tanks2438@outlook.kr>

--- test/change_mem_block_size/run-qemu.sh
+++ test/change_mem_block_size/run-qemu.sh
@@ -1,7 +1,13 @@
 # run-qemu.sh
 #!/bin/bash
 qemu-system-x86_64 \
-  -m 1024 \
+  -machine q35,accel=tcg \
+  -smp 2 \
+  -m 512M,slots=4,maxmem=2G \
+  -object memory-backend-ram,id=ram1,size=1G \
+  -device pc-dimm,id=dimm1,memdev=ram1 \
+  -object memory-backend-ram,id=ram2,size=512M \
+  -device pc-dimm,id=dimm2,memdev=ram2 \
   -kernel ./bzImage \
   -initrd ./initramfs.cpio.gz \
   -append "console=ttyS0 memblksizeorder=29" \
