38560c8a796c981c6c8b2f23276b4683c00bc8c2
Yusun Choi <tanks2438@outlook.kr>
2025-06-01 12:27:20 +0900

    test change_mem_block_size: Change patch file
    Signed-off-by: Yusun Choi <tanks2438@outlook.kr>

--- test/change_mem_block_size/run-qemu.sh
+++ test/change_mem_block_size/run-qemu.sh
@@ -4,11 +4,11 @@ qemu-system-x86_64 \
   -machine q35,accel=tcg \
   -smp 2 \
   -m 1G,slots=4,maxmem=4G \
-  -object memory-backend-ram,id=ram1,size=2G \
+  -object memory-backend-ram,id=ram1,size=1G \
   -device pc-dimm,id=dimm1,memdev=ram1 \
-  -object memory-backend-ram,id=ram2,size=1G \
+  -object memory-backend-ram,id=ram2,size=2G \
   -device pc-dimm,id=dimm2,memdev=ram2 \
   -kernel ./bzImage \
   -initrd ./initramfs.cpio.gz \
-  -append "console=ttyS0 memblksizeorder=29" \
+  -append "console=ttyS0 memblksize=512M root=/dev/ram0 rw" \
   -nographic
